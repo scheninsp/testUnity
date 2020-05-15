676ed81a72b888a020a747b8149b925dee789201
scheninsp <songchen7788@163.com>
2020-05-15 17:01:11 +0800

    fps
    fps

---
+++ TutorialProjects/Nucleon_and_FPSCounter/Assets/Scenes/FPSCounter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+    public int frameRange = 60;
+
+    public int FPS { get; private set; }
+
+    public int AverageFPS { get; private set; }
+    int[] fpsBuffer;
+    int fpsBufferIndex;
+
+
+    void InitializeBuffer()
+    {
+        if (frameRange <= 0) { frameRange = 1; }
+        fpsBuffer = new int[frameRange];
+        fpsBufferIndex = 0;
+    }
+
+    private void Awake()
+    {
+        InitializeBuffer();
+    }
+
+    void Update()
+    {
+        FPS = (int)(1f / Time.unscaledDeltaTime);
+        if(fpsBuffer == null || fpsBuffer.Length != frameRange)
+        {
+            InitializeBuffer();
+        }
+        if (fpsBufferIndex < frameRange ) {
+            fpsBuffer[fpsBufferIndex++] = FPS;
+        }
+        else
+        {
+            fpsBufferIndex = 0;
+        }
+        int sum = 0;
+        for(int i=0; i<frameRange; i++)
+        {
+            sum += fpsBuffer[i];
+        }
+        AverageFPS = sum / frameRange;
+
+    }
+}
