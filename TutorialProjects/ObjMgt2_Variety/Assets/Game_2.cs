8eef58856a663a5b055952d8e1a74e77de13340d
scheninsp <songchen7788@163.com>
2020-05-18 23:14:24 +0800

    object manage
    object manage

---
+++ TutorialProjects/ObjMgt2_Variety/Assets/Game_2.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Game_2 : PersistableObject
+{
+    public PersistableStorage storage;
+
+    public ShapeFactory shapeFactory;
+
+    public KeyCode createKey = KeyCode.C;
+    public KeyCode newGameKey = KeyCode.N;
+    public KeyCode saveKey = KeyCode.S;
+    public KeyCode loadKey = KeyCode.L;
+
+    List<Shape> shapes;
+
+    const int saveVersion = 1;
+
+    private void Awake()
+    {
+        shapes = new List<Shape>();
+        Debug.Log(Application.persistentDataPath);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(createKey))
+        {
+            CreateShape();
+        }
+        else if (Input.GetKey(newGameKey))
+        {
+            BeginNewGame();
+        }
+        else if (Input.GetKey(saveKey))
+        {
+            storage.Save(this);
+        }
+        else if (Input.GetKey(loadKey))
+        {
+            BeginNewGame();
+            storage.Load(this);
+        }
+    }
+
+    void BeginNewGame()
+    {
+        for (int i = 0; i < shapes.Count; i++)
+        {
+            Destroy(shapes[i].gameObject);
+        }
+        shapes.Clear();
+    }
+
+    void CreateShape()
+    {
+        Shape o = shapeFactory.GetRandom();
+        Transform t = o.transform;
+        t.localPosition = Random.insideUnitSphere * 5f;
+        t.localRotation = Random.rotation;
+        t.localScale = Vector3.one * Random.Range(0.1f, 1f);
+        o.SetColor(Random.ColorHSV(hueMin : 0f, hueMax : 1f, 
+            saturationMin : 0.5f, saturationMax : 1f, valueMin : 0.25f,valueMax : 1f,
+            alphaMin : 1f, alphaMax : 1f));
+        shapes.Add(o);
+        //Debug.Log("created : " + o.ShapeId);
+
+    }
+
+    public override void Save(GameDataWriter writer)
+    {
+        writer.Write(saveVersion);
+        writer.Write(shapes.Count);
+        for (int i = 0; i < shapes.Count; i++)
+        {
+            //Debug.Log("writing : " + shapes[i].ShapeId);
+            writer.Write(shapes[i].ShapeId);
+            writer.Write(shapes[i].MaterialId);
+            shapes[i].Save(writer);
+        }
+    }
+
+    public override void Load(GameDataReader reader)
+    {
+        Debug.Log("Loading...");
+        int version = reader.ReadInt();
+        if(version > saveVersion)
+        {
+            Debug.LogError("Unsupported future save version " + version);
+            return;
+        }
+
+        int count = reader.ReadInt();
+        //Debug.Log(count);
+
+        for (int i = 0; i < count; i++)
+        {
+            int shapeId = reader.ReadInt();
+            int materialId = reader.ReadInt();
+            // Debug.Log(shapeId);
+
+            Shape o = shapeFactory.Get(shapeId, materialId);
+            o.Load(reader);
+            shapes.Add(o);
+        }
+    }
+}
---
+++ TutorialProjects/ObjMgt2_Variety/Assets/ShapeFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ShapeFactory :ScriptableObject
+{
+    [SerializeField]
+    Shape[] prefabs;
+
+    [SerializeField]
+    Material[] materials;
+
+    public Shape Get(int shapeId = 0, int materialId = 0)
+    {
+        Shape inst = Instantiate(prefabs[shapeId]);
+        inst.ShapeId = shapeId;
+        inst.SetMaterial(materials[materialId], materialId);
+        return inst;
+    }
+
+    public Shape GetRandom()
+    {
+        return Get(Random.Range(0, prefabs.Length),
+            Random.Range(0, materials.Length));
+    }
+}
