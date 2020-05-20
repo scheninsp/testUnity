70357728f6f1e191875297b02cf2bf533f6d6233
scheninsp <songchen7788@163.com>
2020-05-21 01:29:10 +0800

    multizone
    multizone

---
+++ TutorialProjects/ObjMgt5_SpawnZones/Assets/GameLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLevel : MonoBehaviour
+{
+    [SerializeField]
+    SpawnZone spawnZone;
+
+    void Start()
+    {
+        // Debug.Log(spawnZone.name);
+       // Debug.Log(Game_2.Instance.SpawnZoneOfLevel);
+        Game_2.Instance.SpawnZoneOfLevel = spawnZone;
+    }
+
+
+}
