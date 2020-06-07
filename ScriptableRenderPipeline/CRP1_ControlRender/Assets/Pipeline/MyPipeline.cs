776d238a34c422e48e9c9a10d39c3c4b263e5866
scheninsp <songchen7788@163.com>
2020-06-07 23:07:45 +0800

    srp1
    srp1

---
+++ ScriptableRenderPipeline/CRP1_ControlRender/Assets/Pipeline/MyPipeline.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Experimental.Rendering;
+
+public class MyPipeline : RenderPipeline
+{
+    public override void Render(
+    ScriptableRenderContext renderContext, Camera[] cameras)
+    {
+        base.Render(renderContext, cameras);
+
+        foreach (var camera in cameras)
+        {
+            Render(renderContext, camera);
+        }
+    }
+
+    void Render(ScriptableRenderContext context, Camera camera)
+    {
+        //Culling
+        ScriptableCullingParameters cullingParameters;
+        if (!CullResults.GetCullingParameters(camera, out cullingParameters))
+        {
+            return;
+        }
+        CullResults cull = CullResults.Cull(ref cullingParameters, context);
+
+        //Setup Camera parameters
+        context.SetupCameraProperties(camera);  //set MVP matrix, etc.
+
+        //Setup Command Buffer and Execute
+        var buffer = new CommandBuffer { name = camera.name };
+        CameraClearFlags clearFlags = camera.clearFlags;
+        buffer.ClearRenderTarget(
+            (clearFlags & CameraClearFlags.Depth) != 0,
+            (clearFlags & CameraClearFlags.Color) != 0,
+            camera.backgroundColor
+        );
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Release();
+
+        //Draw Opaque
+        var drawSettings = new DrawRendererSettings(
+            camera, new ShaderPassName("SRPDefaultUnlit"));
+        drawSettings.sorting.flags = SortFlags.CommonOpaque;
+
+        var filterSettings = new FilterRenderersSettings(true)
+        {
+            renderQueueRange = RenderQueueRange.opaque
+        };
+
+        context.DrawRenderers(
+            cull.visibleRenderers, ref drawSettings, filterSettings);
+
+        //Draw Skybox
+        context.DrawSkybox(camera);
+
+        //Draw Transparent
+        drawSettings.sorting.flags = SortFlags.CommonTransparent;  
+        filterSettings.renderQueueRange = RenderQueueRange.transparent;
+        context.DrawRenderers(
+            cull.visibleRenderers, ref drawSettings, filterSettings
+        );
+
+        context.Submit();
+    }
+}
