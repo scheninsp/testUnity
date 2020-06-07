9be67176028e5d4170b8a7c68c589ce02c60838e
scheninsp <songchen7788@163.com>
2020-06-08 01:22:26 +0800

    srp1
    srp1

--- ScriptableRenderPipeline/CRP1_ControlRender/Assets/Pipeline/MyPipeline.cs
+++ ScriptableRenderPipeline/CRP1_ControlRender/Assets/Pipeline/MyPipeline.cs
@@ -1,9 +1,16 @@
 ﻿using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Experimental.Rendering;
+using Conditional = System.Diagnostics.ConditionalAttribute;
 
 public class MyPipeline : RenderPipeline
 {
+    CullResults cull;  //fix here to reduce GC consumption
+
+    CommandBuffer cameraBuffer = new CommandBuffer { name = "Render Camera" };
+
+    Material errorMaterial;  //error fallback
+
     public override void Render(
     ScriptableRenderContext renderContext, Camera[] cameras)
     {
@@ -23,23 +30,35 @@ public class MyPipeline : RenderPipeline
         {
             return;
         }
-        CullResults cull = CullResults.Cull(ref cullingParameters, context);
+
+#if UNITY_EDITOR
+        //add UI to SceneWindow
+        if (camera.cameraType == CameraType.SceneView)
+        {
+            ScriptableRenderContext.EmitWorldGeometryForSceneView(camera);
+        }
+#endif
+
+        CullResults.Cull(ref cullingParameters, context, ref cull);
 
         //Setup Camera parameters
         context.SetupCameraProperties(camera);  //set MVP matrix, etc.
 
         //Setup Command Buffer and Execute
-        var buffer = new CommandBuffer { name = camera.name };
         CameraClearFlags clearFlags = camera.clearFlags;
-        buffer.ClearRenderTarget(
+          
+        cameraBuffer.ClearRenderTarget(
             (clearFlags & CameraClearFlags.Depth) != 0,
             (clearFlags & CameraClearFlags.Color) != 0,
             camera.backgroundColor
         );
-        context.ExecuteCommandBuffer(buffer);
-        buffer.Release();
 
-        //Draw Opaque
+        cameraBuffer.BeginSample("Render Camera");  //start a sub-level
+
+        context.ExecuteCommandBuffer(cameraBuffer);
+        cameraBuffer.Clear();  
+
+        //Draw Opaque, Unlit Shader
         var drawSettings = new DrawRendererSettings(
             camera, new ShaderPassName("SRPDefaultUnlit"));
         drawSettings.sorting.flags = SortFlags.CommonOpaque;
@@ -51,6 +70,7 @@ public class MyPipeline : RenderPipeline
 
         context.DrawRenderers(
             cull.visibleRenderers, ref drawSettings, filterSettings);
+	
 
         //Draw Skybox
         context.DrawSkybox(camera);
@@ -61,7 +81,43 @@ public class MyPipeline : RenderPipeline
         context.DrawRenderers(
             cull.visibleRenderers, ref drawSettings, filterSettings
         );
+		
+		//Conditions for default pipelines
+        DrawDefaultPipeline(context, camera);
+
+        cameraBuffer.EndSample("Render Camera");   //end the sub-level
+        context.ExecuteCommandBuffer(cameraBuffer);
+        cameraBuffer.Clear();
 
         context.Submit();
     }
+
+    [Conditional ("UNITY_EDITOR"), Conditional("DEVELOPEMENT_BUILD")]
+    void DrawDefaultPipeline(ScriptableRenderContext context, Camera camera) {
+        if (errorMaterial == null)
+        {
+            Shader errorShader = Shader.Find("Hidden/InternalErrorShader");
+            errorMaterial = new Material(errorShader)
+            {
+                hideFlags = HideFlags.HideAndDontSave
+            };
+        }
+
+        var drawSettings = new DrawRendererSettings(
+            camera, new ShaderPassName("ForwardBase"));
+        drawSettings.SetShaderPassName(1, new ShaderPassName("PrepassBase"));
+        drawSettings.SetShaderPassName(2, new ShaderPassName("Always"));
+        drawSettings.SetShaderPassName(3, new ShaderPassName("Vertex"));
+        drawSettings.SetShaderPassName(4, new ShaderPassName("VertexLMRGBM"));
+        drawSettings.SetShaderPassName(5, new ShaderPassName("VertexLM"));
+
+        drawSettings.SetOverrideMaterial(errorMaterial, 0);  //errorMaterial use ForwardBase
+
+        var filterSettings = new FilterRenderersSettings(true);
+
+        context.DrawRenderers(
+            cull.visibleRenderers, ref drawSettings, filterSettings);
+
+    }
+
 }
