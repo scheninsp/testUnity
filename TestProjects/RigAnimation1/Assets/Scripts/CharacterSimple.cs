a0f0e4dd356c85e8367e6834f3ed268ca0d8a369
scheninsp <songchen7788@163.com>
2020-08-24 03:05:32 +0800

    UTS2 material
    UTS2 material

---
+++ TestProjects/RigAnimation1/Assets/Scripts/CharacterSimple.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSimple : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        transform.Rotate(Vector3.up, 1f);
+    }
+}
---
+++ TestProjects/RigAnimation1/Assets/Scripts/DirectionalLightBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalLightBehavior : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        //transform.Rotate(Vector3.up, 5f, Space.World);
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x,
+            transform.localEulerAngles.y + 1f,
+            transform.localEulerAngles.z);
+    }
+}
